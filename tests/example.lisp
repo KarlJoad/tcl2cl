496d6fed5b3756c4357c1456d5395ae8f751da69
Karl Hallsby <karl@hallsby.com>
2023-12-10 18:10:02 -0600

    Correct example test's defpackage

--- tests/example.lisp
+++ tests/example.lisp
@@ -1,7 +1,7 @@
-(defpackage :chil/tests
+(defpackage :tcl2cl/tests
   (:use :cl :lisp-unit2))
 
-(in-package :chil/tests)
+(in-package :tcl2cl/tests)
 
 (define-test example ()
   (assert-eql 1 (- 2 1)))
